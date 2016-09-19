cbf18701424ed66a5a48dcabf883b7ce7f531dc6
Robert Ang <angrobertsh@gmail.com>
2016-09-18 22:33:40 -0700

    working off of Jangmi's skeleton

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  context: __dirname,
+  entry: "./lib/watercolor.js",
+  output: {
+    path: "./lib/bundle/",
+    publicPath: "/lib/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: 'node-loader'
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
